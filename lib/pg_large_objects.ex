805b33b1fe2d8fb58a0fd1419f319afc83314971
Frerich Raabe <frerich.raabe@gmail.com>
2025-11-08 10:12:57 +0100

    mix new pg_large_objects

---
+++ lib/pg_large_objects.ex
@@ -0,0 +1,18 @@
+defmodule PgLargeObjects do
+  @moduledoc """
+  Documentation for `PgLargeObjects`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PgLargeObjects.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pg_large_objects_test.exs
@@ -0,0 +1,8 @@
+defmodule PgLargeObjectsTest do
+  use ExUnit.Case
+  doctest PgLargeObjects
+
+  test "greets the world" do
+    assert PgLargeObjects.hello() == :world
+  end
+end
