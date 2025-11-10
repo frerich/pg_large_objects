fc5ae4ebd2b56fe4cacd9c6dbbf1e6198b35d914
Frerich Raabe <frerich.raabe@gmail.com>
2025-11-10 19:04:36 +0100

    More tests for PgLargeObjects

--- test/pg_large_objects_test.exs
+++ test/pg_large_objects_test.exs
@@ -15,6 +15,23 @@ defmodule PgLargeObjectsTest do
 
       assert data == get_large_object!(oid)
     end
+
+    test "can import from enumerable" do
+      data = :crypto.strong_rand_bytes(Enum.random(0..1024))
+
+      {:ok, pid} = StringIO.open(data, encoding: :latin1)
+
+      stream = IO.binstream(pid, 3)
+
+      {:ok, oid} =
+        TestRepo.transact(fn ->
+          PgLargeObjects.import(TestRepo, stream)
+        end)
+
+      assert data == get_large_object!(oid)
+
+      {:ok, _output} = StringIO.close(pid)
+    end
   end
 
   describe "export/3" do
@@ -23,10 +40,31 @@ defmodule PgLargeObjectsTest do
 
       oid = put_large_object!(data)
 
-      assert {:ok, ^data} =
-               TestRepo.transact(fn ->
-                 PgLargeObjects.export(TestRepo, oid)
-               end)
+      TestRepo.transact(fn ->
+        assert {:ok, ^data} = PgLargeObjects.export(TestRepo, oid)
+      end)
+    end
+
+    test "can export data to collectable" do
+      data = :crypto.strong_rand_bytes(Enum.random(0..1024))
+
+      oid = put_large_object!(data)
+
+      {:ok, pid} = StringIO.open("", encoding: :latin1)
+      stream = IO.binstream(pid, 3)
+
+      TestRepo.transact(fn ->
+        assert :ok == PgLargeObjects.export(TestRepo, oid, into: stream)
+        {:ok, nil}
+      end)
+
+      assert {"", ^data} = StringIO.contents(pid)
+    end
+
+    test "handles invalid object IDs" do
+      TestRepo.transact(fn ->
+        assert {:error, :invalid_oid} = PgLargeObjects.export(TestRepo, 12345)
+      end)
     end
   end
 end
