dddce0f2116cf9f5b4b19f220e7713c7a088b32c
Frerich Raabe <frerich.raabe@gmail.com>
2025-11-11 21:23:59 +0100

    Extend tests for PgLargeObjects.Repo

--- test/pg_large_objects/repo_test.exs
+++ test/pg_large_objects/repo_test.exs
@@ -13,6 +13,24 @@ defmodule PgLargeObjects.RepoTest do
 
       assert data == get_large_object!(oid)
     end
+
+    test "can import from enumerable" do
+      data = :crypto.strong_rand_bytes(Enum.random(0..1024))
+
+      {:ok, pid} = StringIO.open(data, encoding: :latin1)
+
+      stream = IO.binstream(pid, 3)
+
+      {:ok, oid} =
+        TestRepo.transaction(fn ->
+          {:ok, oid} = TestRepo.import_large_object(stream)
+          oid
+        end)
+
+      assert data == get_large_object!(oid)
+
+      {:ok, _output} = StringIO.close(pid)
+    end
   end
 
   describe "export_large_object/1" do
@@ -26,6 +44,21 @@ defmodule PgLargeObjects.RepoTest do
                  {:ok, data} = TestRepo.export_large_object(oid)
                  data
                end)
+    end
+
+    test "can export data to collectable" do
+      data = :crypto.strong_rand_bytes(Enum.random(0..1024))
+
+      oid = put_large_object!(data)
+
+      {:ok, pid} = StringIO.open("", encoding: :latin1)
+      stream = IO.binstream(pid, 3)
+
+      TestRepo.transaction(fn ->
+        assert :ok == TestRepo.export_large_object(oid, into: stream)
+      end)
+
+      assert {"", ^data} = StringIO.contents(pid)
     end
   end
 end
