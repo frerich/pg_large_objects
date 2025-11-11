23b65c66557db7c3272f100a4c836c7d688bdc20
Frerich Raabe <frerich.raabe@gmail.com>
2025-11-11 20:33:26 +0100

    Add documentation for PgLargeObjects.EctoQuery
    For now, mostly reference the PostgreSQL documentation for details.

--- lib/pg_large_objects/ecto_query.ex
+++ lib/pg_large_objects/ecto_query.ex
@@ -24,54 +24,99 @@ defmodule PgLargeObjects.EctoQuery do
   of these functions.
   """
 
+  @doc """
+  Create large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-CREATE for details.
+  """
   defmacro lo_create(desired_oid \\ 0) do
     quote do
       fragment("lo_create(?)", unquote(desired_oid))
     end
   end
 
+  @doc """
+  Delete large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-UNLINK for details.
+  """
   defmacro lo_unlink(oid) do
     quote do
       fragment("lo_unlink(?)", unquote(oid))
     end
   end
 
+  @doc """
+  Open large object for reading or writing.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-OPEN for details.
+  """
   defmacro lo_open(oid, flags) do
     quote do
       fragment("lo_open(?, ?)", unquote(oid), unquote(flags))
     end
   end
 
+  @doc """
+  Close large object file descriptor.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-CLOSE for details.
+  """
   defmacro lo_close(fd) do
     quote do
       fragment("lo_close(?)", unquote(fd))
     end
   end
 
+  @doc """
+  Write data to large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-WRITE for details.
+  """
   defmacro lo_write(fd, data) do
     quote do
       fragment("lowrite(?, ?)", unquote(fd), unquote(data))
     end
   end
 
+  @doc """
+  Read data from large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-READ for details.
+  """
   defmacro lo_read(fd, length \\ 1_048_576) do
     quote do
       fragment("loread(?, ?)", unquote(fd), unquote(length))
     end
   end
 
+  @doc """
+  Adjust read/write position in large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-SEEK for details.
+  """
   defmacro lo_lseek64(fd, offset, whence) do
     quote do
       fragment("lo_lseek64(?, ?, ?)", unquote(fd), unquote(offset), unquote(whence))
     end
   end
 
+  @doc """
+  Get read/write position of large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-TELL for details.
+  """
   defmacro lo_tell64(fd) do
     quote do
       fragment("lo_tell64(?)", unquote(fd))
     end
   end
 
+  @doc """
+  Truncate or expand large object.
+
+  See https://www.postgresql.org/docs/current/lo-interfaces.html#LO-TRUNCATE for details.
+  """
   defmacro lo_truncate64(fd, size) do
     quote do
       fragment("lo_truncate64(?, ?)", unquote(fd), unquote(size))
